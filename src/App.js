fdd0cc9409f445fe504394233f120c7ef3bb4f97
iamjinpark <wldls7754@gmail.com>
2024-07-23 00:03:09 +0900

    feat : 3강 state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
+import MyComponent from './MyComponent';
+import Say from './Say';
 import './App.css';
 
 function App() {
-  // const name = undefined;
-  const name = '지인';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: '16px',
-  };
-
   return (
-    <div>
-      <div className="react">
-        {/* {name === "지인" ? <h1>지인아 안녕!</h1> : null} */}
-        {/* {name === "지인" && <h1>지인아 안녕!</h1>} */}
-        {name || '지인'}
-      </div>
-      <input />
-    </div>
+    <>
+      {/* <MyComponent name="react" favoriteNumber={9}>
+        리액트
+      </MyComponent> */}
+      <Say />
+    </>
   );
 }
 
