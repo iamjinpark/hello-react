09f434c49c34a18258056a27f2e1db504749b5d2
iamjinpark <wldls7754@gmail.com>
2024-10-01 22:19:22 +0900

    feat : 5강 ref 실습

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import './App.css';
 import MyComponent from './3.Component/MyComponent';
 import Say from './3.Component/Say';
 import EventPractice from './4.EventHandling/EventPractice.jsx';
+import ValidationSample from './5.Ref/ValidationSample.jsx';
+import ScrollBox from './5.Ref/ScrollBox.jsx';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <>
-      {/* <MyComponent name="react" favoriteNumber={9}>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <MyComponent name="react" favoriteNumber={9}>
         리액트
       </MyComponent> */}
-      {/* <Say /> */}
-      <EventPractice />
-    </>
-  );
+        {/* <Say /> */}
+        {/* <EventPractice /> */}
+        {/* <ValidationSample /> */}
+
+        <ScrollBox
+          ref={(ref) => {
+            this.ScrollBox = ref;
+          }}
+        />
+        <button onClick={() => this.ScrollBox.scrollBottom()}>맨밑으로</button>
+      </div>
+    );
+  }
 }
 
 export default App;
