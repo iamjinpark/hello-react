b76f1be126ab666de6cadff5d0abba4f5e44b210
iamjinpark <wldls7754@gmail.com>
2024-10-02 16:19:33 +0900

    feat : 7강 컴포넌트 라이프사이클 메서드 실습

--- src/App.js
+++ src/App.js
@@ -6,8 +6,23 @@ import EventPractice from './4.EventHandling/EventPractice.jsx';
 import ValidationSample from './5.Ref/ValidationSample.jsx';
 import ScrollBox from './5.Ref/ScrollBox.jsx';
 import IterationSample from './6. Iteration/IterationSample.jsx';
+import LifeCycleSample from './7.LifeCycle/LifeCycleSample.jsx';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
@@ -27,7 +42,10 @@ class App extends Component {
         />
         <button onClick={() => this.ScrollBox.scrollBottom()}>맨밑으로</button> */}
 
-        <IterationSample />
+        {/* <IterationSample /> */}
+
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
       </div>
     );
   }
